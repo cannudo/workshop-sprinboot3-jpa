12afb8165c1d750195cef1dfa3da1754b3568e13
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-12-03 20:53:58 -0300

    :card_file_box: feat: termina a modelagem das classes

--- curso/src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ curso/src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.educandoweb.curso.config;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.curso.entities.Order;
 import com.educandoweb.curso.entities.User;
+import com.educandoweb.curso.repositories.OrderRepository;
 import com.educandoweb.curso.repositories.UserRepository;
 
 @Configuration
@@ -17,6 +21,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		User lis = new User(null, "NGeremaias", "pedreiro@viladochaves.com", "40028922", "LisComS");
@@ -24,6 +31,12 @@ public class TestConfig implements CommandLineRunner {
 		List<User> sacoDeFarinha = new ArrayList<>();
 		sacoDeFarinha.add(lis);
 		sacoDeFarinha.add(liz);
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), lis);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), liz);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), lis);
+		
 		userRepository.saveAll(sacoDeFarinha);
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
--- curso/src/main/java/com/educandoweb/curso/entities/Order.java
+++ curso/src/main/java/com/educandoweb/curso/entities/Order.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -19,7 +22,9 @@ public class Order implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
 	@ManyToOne
@@ -31,7 +36,7 @@ public class Order implements Serializable {
 		
 	}
 
-	public Order(long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
