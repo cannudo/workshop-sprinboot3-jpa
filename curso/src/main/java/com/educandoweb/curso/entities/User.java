83200bc24b01f4c232f818caac129ffeffb4ffdb
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-12-03 19:56:07 -0300

    :construction: feat: começa a modelar a classe User e Order

--- curso/src/main/java/com/educandoweb/curso/entities/User.java
+++ curso/src/main/java/com/educandoweb/curso/entities/User.java
@@ -1,12 +1,15 @@
 package com.educandoweb.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy  = "client")
+	private List<Order> orders = new ArrayList<>();
+	/* Um cliente (User) tem, no mínimo, nenhum pedido (Order) e, no máximo, vários pedidos (Order) */
 	
 	public User() {}
 
@@ -72,6 +79,12 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	@OneToMany(mappedBy = "client")
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 
 	@Override
 	public int hashCode() {
@@ -88,13 +101,5 @@ public class User implements Serializable {
 			return false;
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
-				+ "]";
-	}
-	
-	
+	}	
 }
