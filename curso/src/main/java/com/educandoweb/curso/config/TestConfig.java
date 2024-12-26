3a0c73bdc9cb8f159b444fc2a26e110f7604ce9f
Luan da Costa Redmann <luandacostaredmann@gmail.com>
2024-12-26 15:41:23 -0300

    Surtando

--- curso/src/main/java/com/educandoweb/curso/config/TestConfig.java
+++ curso/src/main/java/com/educandoweb/curso/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
 		User lis = new User(null, "NGeremaias", "pedreiro@viladochaves.com", "40028922", "LisComS");
 		User liz = new User(null, "Liziane", "sainhadatj@provas.com", "22982004", "LizComZ");
 		List<User> sacoDeFarinha = new ArrayList<>();
